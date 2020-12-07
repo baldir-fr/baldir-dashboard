6c0c85ca23125de0deedf0fe0cb08ad238b94980
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-08 00:16:38 +0100

    ci: WIP trying to generate pickles doc

--- src/scripts/powershell/generate-pickles-documentation.ps1
+++ src/scripts/powershell/generate-pickles-documentation.ps1
@@ -1,11 +1,14 @@
 #!/usr/bin/env pwsh
-. .\shared-functions.ps1
 
-$FeatureDirectory="c:/dogfooding/baldir-dashboard/desktop-infra/src/jvmTest/resources/features"
+# Should be called from the root of the repository
+
+. src/scripts/powershell/shared-functions.ps1
+
+$FeatureDirectory="desktop-infra/src/jvmTest/resources/features"
 # Directory where output files will be placed
-$OutputDirectory="c:/dogfooding/baldir-dashboard/desktop-infra/build/reports/acceptance/pickles"
+$OutputDirectory="desktop-infra/build/reports/acceptance/pickles"
 # Can be a comma separated list
-$CucumberJsonTestResultsFiles="c:/dogfooding/baldir-dashboard/desktop-infra/build/reports/acceptance/cucumberReport.json"
+$CucumberJsonTestResultsFiles="desktop-infra/build/reports/acceptance/cucumberReport.json"
 $SystemUnderTestName="Baldir-dashboard"
 # --df documentation format
 # DHTML = dynamic HTML (with search)
@@ -17,18 +20,17 @@ $SystemUnderTestName="Baldir-dashboard"
 $DocumentationFormat="DHTML"
 # --trfmt - the format of the linked test results
 # nunit|nunit3|xunit|xunit2|mstest|cucumberjson|specrun|vstest
-# -l language
-$Language="fr"
+
 if (Test-CommandExists("pickles"))
 {
     Write-Output "Generating living documentation with pickles..."
-    Pickles -l=$Language --sn=$SystemUnderTestName --df=$DocumentationFormat --trfmt=cucumberjson --lr=$CucumberJsonTestResultsFile --feature-directory=$FeatureDirectory --output-directory=$OutputDirectory
+    Pickles --sn=$SystemUnderTestName --df=$DocumentationFormat --trfmt=cucumberjson --lr=$CucumberJsonTestResultsFile --feature-directory=$FeatureDirectory --output-directory=$OutputDirectory
     # Invoke-Command -ScriptBlock {}
     if($LASTEXITCODE -ne 0) {
         Write-Output "Could not generate living documentation with pickles."
         EXIT 1
     }
-
+    Write-Output "Living documentation generated in $OutputDirectory"
 }
 else
 {
